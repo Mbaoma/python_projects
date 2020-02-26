2e2fb44cd6823cb8191c36dad27dccde4d7077d4
Mbaoma Chioma (Omarrr) <49791498+Mbaoma@users.noreply.github.com>
2020-02-27 00:42:05 +0100

    Update iteration.py

--- iteration.py
+++ iteration.py
@@ -1,26 +1,4 @@
 '''
-In python, a variable can be assigned more than one value'''
-a= 8
-print(a)
-a= 9
-print(a)
-print('--------------')
-
-'''An update on a variable can also be done'''
-x= 3
-x= x+1
-print(x)
-print('----------------')
-
-''' While statements are used for repetitive tasks, the underlying code prints Sam for everytime the value of the variable s, is greater than 0'''
-def the_one(s):
-	while s > 0:
-		print('Sam')
-		s= s-1
-the_one(6)
-print('----------------')
-
-'''
 A Newtonian equation to solve for square root of a number- y=x+(a/x)/2
 '''
 while True:
