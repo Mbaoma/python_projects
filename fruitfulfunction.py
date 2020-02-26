6706630253149a49962948596b0639c3be2bccc7
Mbaoma Chioma (Omarrr) <49791498+Mbaoma@users.noreply.github.com>
2020-02-27 00:38:35 +0100

    Update fruitfulfunction.py

--- fruitfulfunction.py
+++ fruitfulfunction.py
@@ -1,11 +1,14 @@
+#Prints out the absolute value of numbers 
 def absolute(x):
 	if x < 0:
 		print (-x)
 	else:
 		print(x)
 absolute(-6)
+absolutes(7)
 print("--------------------")
-''' write a compare function that returns
+
+'''write a compare function that returns
  1, for x>y
  0, for x==y
  -1, for x<y
@@ -20,13 +23,8 @@ def compare(x,y):
 x,y= eval(input('enter a number: '))
 compare(x,y)
 print('-------------------')
-def GOAT():
-	topdawg= 'Lamar'
-	print(topdawg)
-GOAT()
-print('-------------------')
-'''
-boolean function
+
+'''Boolean function:
 boolean is an inbuilt python function that prints True or False depending on the given condition '''
 print(4%2 == 0)
 print(6%4 == 0)
@@ -36,7 +34,5 @@ def yesno(a,b):
 		print('True')
 	else:
 		print('False')
-a,b= eval(input('enter 2 numbers seperated by comma'))
+a,b= eval(input('enter 2 numbers seperated by comma: '))
 yesno(a,b)
-print('-------------------')
-
