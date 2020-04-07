9a71fd1f0e9d1f4407daeb67d5c5ab468fd13343
Mbaoma Chioma (Omarrr) <49791498+Mbaoma@users.noreply.github.com>
2020-04-07 10:47:49 +0100

    Update forty_questions.py

--- forty_questions.py
+++ forty_questions.py
@@ -6,10 +6,10 @@ import random
 import pprint
 
 def question_numbers(l,players):
-    for i in range(0, 40, players):
+    for i in range(0, 40, players): #range(start, stop (range of question numbers), number of players)
         yield l[i: i + players]
-players = 8
-names = ['weirdo','shey proper','omarrr','aunt chizi','vykybobo']
+players = 8 #number of players
+names = ['weirdo','shey proper','omarrr','aunt chizi','vykybobo'] #players names
 random.shuffle(names)
 num = list(question_numbers(range(1,40,), players))
 random.shuffle(num)
