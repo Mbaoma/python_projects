f9b2bd0909c278b03e01e3fa1a9eac1bbae2bdf7
Mbaoma Chioma (Omarrr) <49791498+Mbaoma@users.noreply.github.com>
2020-05-24 19:12:24 +0100

    Add files via upload

---
+++ quarantined-devs/quarantined-devs.py
@@ -0,0 +1,38 @@
+#a script that randomly selects 4 users for an AMA session
+import random
+interviewed = []
+def quarantined_devs(names):
+    get_names()
+    interviewed_folks()
+    uninterviewed_folks()
+
+def get_names():
+    for i in name_file:
+        i = i.split()
+        names.append(i)
+
+def interviewed_folks():
+    while len(interviewed) < len(names):
+        to_be_interviewed = random.sample(names, 3)
+        for i in to_be_interviewed: 
+            interviewed.append(i)
+
+        interviewed_set = []
+        for i in interviewed:
+            if i not in interviewed_set:
+                interviewed_set.append(i)
+        print('INTERVIEWED FOLKS')
+        print(interviewed_set)
+
+def uninterviewed_folks():
+    not_interviewed = []
+    for i in names:
+        if i not in interviewed:
+            not_interviewed.append(i)
+    print('UNINTERVIEWED FOLKS')        
+    print(not_interviewed)
+    
+name_file = open("C:/Users/User/Desktop/OMARRR/Codes/quarantined-devs/dev-names.csv", "r")
+names = []
+
+quarantined_devs(names)
